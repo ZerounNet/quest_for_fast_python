53cc1201d6dcc9b38aa53ebf7254f45544314102
Anthony Khong <anthony.kusumo.khong@gmail.com>
2018-10-14 22:01:29 +0700

    .

--- ema.pyx
+++ ema.pyx
@@ -1,6 +1,7 @@
 cimport cython
 cimport numpy as np
 import numpy as np
+from cytoolz import accumulate
 
 def untyped_ema(xs, smooth, init):
     smoothed = np.empty_like(xs)
@@ -11,8 +12,6 @@ def untyped_ema(xs, smooth, init):
             smoothed[t] = (1-smooth)*xs[t-1] + smooth*smoothed[t-1]
     return smoothed
 
-@cython.boundscheck(False)
-@cython.wraparound(False)
 def typed_ema(
         np.ndarray[np.float64_t, ndim=1] xs,
         double smooth,
@@ -22,10 +21,22 @@ def typed_ema(
     )
     cdef int t
     cdef int length = len(xs)
-    with cython.nogil:
-        for t in range(length):
-            if t == 0:
-                smoothed[t] = init
-            else:
-                smoothed[t] = (1-smooth)*xs[t-1] + smooth*smoothed[t-1]
+    for t in range(length):
+        if t == 0:
+            smoothed[t] = init
+        else:
+            smoothed[t] = (
+                (1-smooth)*xs[t-1] +
+                smooth*smoothed[t-1]
+            )
     return smoothed
+
+cdef ema_step(last_smoothed: double, next_value: double, smooth: double):
+    return (1-smooth)*next_value + smooth*last_smoothed
+
+def reduce_ema(
+        np.ndarray[np.float64_t, ndim=1] xs,
+        double smooth,
+        double init):
+    smoothed = accumulate(lambda s, x: ema_step(s, x, smooth), xs[:-1], init)
+    return np.fromiter(smoothed, dtype=np.float64)
--- numba_example.py
+++ numba_example.py
@@ -1,6 +1,9 @@
 import numpy as np
 from numba import jit
 
+import pyximport; pyximport.install()
+from ema import untyped_ema, typed_ema, reduce_ema
+
 def vanilla_ema(xs, smooth, init):
     smoothed = np.empty_like(xs)
     for t in range(len(xs)):
@@ -25,13 +28,15 @@ def jitted_ema(xs, smooth, init):
 
 
 '''
-import pyximport; pyximport.install()
-from ema import untyped_ema, typed_ema
 
 xs = np.random.randn(10000)
 %timeit _ = vanilla_ema(xs, smooth=0.8, init=0.0)
 %timeit _ = untyped_ema(xs, smooth=0.8, init=0.0)
 %timeit _ = typed_ema(xs, smooth=0.8, init=0.0)
+
+xs = np.random.randn(10000)
+%timeit _ = vanilla_ema(xs, smooth=0.8, init=0.0)
+%timeit _ = reduce_ema(xs, smooth=0.8, init=0.0)
 
 ln -s /opt/anaconda/lib/python3.6/site-packages/numpy/core/include/numpy /usr/local/include/numpy
 '''
